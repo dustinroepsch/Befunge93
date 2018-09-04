13226d0b307a892d839348741c86cd88a92972fa
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2018-09-04 17:42:35 -0500

    Adding arguement parsing via boost

--- main.cpp
+++ main.cpp
@@ -2,10 +2,32 @@
 #include <fstream>
 #include "src/FungeInterpreter.h"
 
-int main() {
-    FungeInterpreter interpreter;
+#include <boost/program_options.hpp>
 
-    std::ifstream file("/Users/dustin/CLionProjects/Funge93/ExamplePrograms/pi.bf");
+int main(int ac, const char* av[]) {
+    namespace po = boost::program_options;
+    po::options_description desc("How to use Befunge93");
+    desc.add_options()
+            ("help", "Produce this help message")
+            ("filepath", po::value<std::string>(), "Path to the befunge program to execute");
+
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    if (!vm.count("filepath")) {
+        std::cout << "use --filepath to tell Befunge93 what program to run." << std::endl;
+        return -1;
+    }
+
+    FungeInterpreter interpreter;
+    std::ifstream file(vm["filepath"].as<std::string>());
     interpreter.LoadProgram(file);
     file.close();
 
