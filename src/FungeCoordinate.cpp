27f4c2e6d5d5469cf33e75b921053fdcaa0fefc5
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2018-09-04 14:27:10 -0500

    wrote FungeCoordinate

---
+++ src/FungeCoordinate.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Dustin Ryan-Roepsch on 9/4/18.
+//
+
+#include "FungeCoordinate.h"
+#include "FungeSpace.h"
+
+int32_t FungeCoordinate::x() {
+    return _x;
+}
+
+int32_t FungeCoordinate::y() {
+    return _y;
+}
+
+void FungeCoordinate::x(int32_t x) {
+    _x = wrap(x, FungeSpace::SPACE_WIDTH);
+}
+
+void FungeCoordinate::y(int32_t y) {
+    _y = wrap(y, FungeSpace::SPACE_HEIGHT);
+}
+
+int32_t FungeCoordinate::wrap(int32_t val, int32_t max) {
+    while (val < 0) {
+        val += max;
+    }
+
+    return val % max;
+}
+
---
+++ src/FungeCoordinate.h
@@ -0,0 +1,31 @@
+//
+// Created by Dustin Ryan-Roepsch on 9/4/18.
+//
+
+#ifndef FUNGE93_FUNGECOORDINATE_H
+#define FUNGE93_FUNGECOORDINATE_H
+
+
+#include <cstdint>
+
+class FungeCoordinate {
+public:
+    FungeCoordinate(int32_t x, int32_t y) : _x(x), _y(y) {}
+
+    int32_t x();
+
+    int32_t y();
+
+    void x(int32_t x);
+
+    void y(int32_t y);
+
+private:
+    int32_t _y;
+    int32_t _x;
+
+    int32_t wrap(int32_t x, int i);
+};
+
+
+#endif //FUNGE93_FUNGECOORDINATE_H
